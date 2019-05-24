a0f3c96c09aa8d179c42b8b8449157cdf80b1ec6
Dizzerin <Dizzerin@gmail.com>
2019-05-24 00:04:42 -0700

    Started working on setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
 # setup.py
+from setuptools import setup
+
+setup(
+   name='HoneyBot',
+   version='1.0',
+   description='HoneyBot module',
+   license="MIT",
+   long_description='HoneyBot is a python-based IRC bot',
+   author='Abdur-Rahmaan Janhangeer',
+   author_email='arj.python@gmail.com',
+   url="https://github.com/Abdur-rahmaanJ/honeybot",
+   packages=['HoneyBot'],
+   install_requires=['', ''],
+   scripts=[]
+)
